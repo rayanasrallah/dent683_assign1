8ee5dcb16a62c94a857dfcdc2e610f86767c8e6b
rayanasrallah <raya.nasrallah@mail.mcgill.ca>
2021-10-18 12:29:54 -0400

    Commit after changes and edits: 1. Fixing wrong imported excel sheet 2. Deleting unnecessary commands 3. Adding submitting requirements (https link and number of participants)

--- Codes/dent683_assign1.R
+++ Codes/dent683_assign1.R
@@ -18,7 +18,7 @@ oh2014 <- read_xlsx(path = here("Data","nhanes_ohx_12_18.xlsx"),
 oh2016 <- read_xlsx(path = here("Data","nhanes_ohx_12_18.xlsx"),
                     sheet = "oh2016")
 oh2018 <- read_xlsx(path = here("Data","nhanes_ohx_12_18.xlsx"),
-                    sheet = "oh2016")
+                    sheet = "oh2018")
 demo2012 <- read_xlsx(path = here("Data","nhanes_demo_12_18.xlsx"),
                     sheet = "demo2012")
 demo2014 <- read_xlsx(path = here("Data","nhanes_demo_12_18.xlsx"),
@@ -36,38 +36,22 @@ demo2018 <- read_xlsx(path = here("Data","nhanes_demo_12_18.xlsx"),
 
 #task 6.2:creating a single data set that includes SEQ and variables ending with CTC from the ohx data-sets
 
-#before creating a single data set, checking that the variables' names are identical between the 4 oh data-set
-identical(names(oh2012), names(oh2014))
-identical(names(oh2012), names(oh2016))
-identical(names(oh2012), names(oh2018))
-#results: identical names between oh2012 and oh2014, but not when compared to oh2016 and oh2018
-
-#now I want to check what are the different variables?
-oh2012vsoh2016 <- setdiff(names(oh2012), names(oh2016))
-oh2012vsoh2018 <- setdiff(names(oh2012), names(oh2018))
-identical(oh2012vsoh2016,oh2012vsoh2018)
-#results: identical (TRUE)
-
-#so the differences between oh2012 (and oh2014 because they are identical) and oh2016 and oh2018 are the same
-#now I will check if the names of oh2016 and oh 2018 are identical
-identical (names(oh2016), names(oh2018))
-#results: identical (TRUE)
-
-#So, in sum: the variables of oh2012 are the same in oh2014
-#            the variables of oh2016 are the same in oh2018
-#            the differences between the sets are the same
-
-#now I want to know how many variables are the same?
+#before creating a single data set, 
+#now I want to know how many variables are the same in the 4 data-sets?
+intersect(names(oh2012), names(oh2014)) %>% length()
+intersect(names(oh2016), names(oh2018)) %>% length()
 intersect(names(oh2012), names(oh2016)) %>% length()
-#So, all the 110 variables in oh2012 (and oh2014 obviously) are included in oh2016 (and oh2018 obviously)
+
+#In sum: 1.all the 110 variables in oh2012 are present in oh2014
+#        2.all the 170 variables in oh2016 are present in oh2018
+#        3.all 110 variables of oh2012 and oh2014 are present in oh2016 and oh2018
 
 #now I want to know if the variables of interest are coded the same way in all the sets before merging
 oh2012 %>% select(ends_with("CTC")) 
 oh2014 %>% select(ends_with("CTC"))
 oh2016 %>% select(ends_with("CTC"))
 oh2018 %>% select(ends_with("CTC"))
-#in a randomly check, they seem to be coded the same, 
-#and there are 28 variables ending with CTC in the 4 data-sets
+#in a randomly check, they seem to be coded the same
 
 #now I will select the variables of interest only for those who completed oral examinations
 #this will be saved in new objects
@@ -89,17 +73,7 @@ view(oh_mrg)
 
 #finally, checking for duplicates in the id variable (SEQ)
 table(duplicated(oh_mrg$SEQN))
-#results: there are 8857 duplicated rows based in SEQN variable
-
-#removing duplicate rows based on SEQN, saving to a new object
-library(dplyr)
-oh_mrg_nodup <- oh_mrg %>% distinct(SEQN, .keep_all = TRUE)
-
-view(oh_mrg_nodup)
-
-#checking for duplicates in SEQN in the final data-set "oh_mrg_nodup"
-table(duplicated(oh_mrg_nodup$SEQN))
-#results: no duplicates in oh_mrg_nodup (FALSE)
+#results: No duplicates (FALSE)
 
 
 #commit+push
@@ -160,7 +134,7 @@ table(duplicated(demo_mrg$SEQN))
 #task 6.5: merging the data sets created in tasks 6.2 and 6.4, ignoring participants who are not present in both data-sets
 #i.e. merging oh_mrg_nodup and demo_mrg by SEQN, a horizontal merging (including participants present on both data-sets)
 
-final_mrg <- merge(demo_mrg, oh_mrg_nodup, by="SEQN", all=FALSE)
+final_mrg <- merge(demo_mrg, oh_mrg, by="SEQN", all=FALSE)
 view(final_mrg)
 
 
@@ -169,4 +143,28 @@ view(final_mrg)
 
 ####################
 
+#task 7: saving the final data-set (final_mrg) as csv file into the DATA sub-folder
+write.csv(final_mrg,here("Data","final_mrg.csv"), row.names =FALSE)
+
+
+####################
+
+#task 8: editing the .gitignore file so Git will not track or push csv and xlsx files
+
+
+####################
+
+#For submitting: 
+# 1.https link for the remote depository: https://github.com/rayanasrallah/dent683_assign1
+# 2.The SHA1 of the final commit  
+# 3.The number of participants in your final data set, by year of survey  
+final_mrg %>% group_by(year) %>% tally ()
+
+view(oh2018_int)
+
+
+#commit+push
+
+
+####################
 
